f5719f28b866a025580acd28b50c68cb0562df9a
Syliel <jbellcollins@gmail.com>
2018-09-26 11:10:01 -0400

    Initial

---
+++ AdventureGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+	// foo
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Look mom, it's notepad++");
+		
+        textComponent.text = ("I am typing stuff!");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ States.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "States")]
+public class States : ScriptableObject {
+
+  [TextArea(10,14)] [SerializeField] string storyText;
+}
+     
